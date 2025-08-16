e160c0771f05f58c74029e4f6f4eb85dd4f77cf0
Hussain <hkosaimi@gmail.com>
2025-08-16 03:53:52 +0300

    new

--- src/redux/queries/apiSlice.js
+++ src/redux/queries/apiSlice.js
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:4001",
+  // baseUrl: "http://localhost:4001",
+  baseUrl: "https://backend-production-9357.up.railway.app",
   credentials: "include",
 });
 
